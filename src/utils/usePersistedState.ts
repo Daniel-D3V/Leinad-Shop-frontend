0924c2ce16041292436086bd9d6eb089f9824922
Daniel-Developement <contatodanielsilvaoficial@gmail.com>
2023-05-31 12:50:53 -0300

    save finish

--- src/utils/usePersistedState.ts
+++ src/utils/usePersistedState.ts
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 
 export const usePersistedState = (key: string, initialState: any) => {
-    const [state, setState] = useState(() => {
-        const storageValue = localStorage.getItem(key);
+    const [state, setState] = useState(initialState)
 
-        if (storageValue) return JSON.parse(storageValue);
+    useEffect(() => {
+        setState(() => {
+            const storageValue = localStorage.getItem(key)
 
-        return initialState;
-    });
+            if (storageValue) return JSON.parse(storageValue);
+
+            return initialState;
+        })
+
+    }, [key, initialState])
 
     useEffect(() => {
         localStorage.setItem(key, JSON.stringify(state))
-    }, [state])
+    }, [state, setState])
 
     return [state, setState];
 }
