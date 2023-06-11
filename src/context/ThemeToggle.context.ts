78232357d18017f24248e49431286ffb838f5ac9
Daniel-Developement <contatodanielsilvaoficial@gmail.com>
2023-06-11 14:44:42 -0300

    Init commit

--- src/context/ThemeToggle.context.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-interface ThemeToggleContextProps {
-    toggleTheme: () => void
-}
-
-export const ThemeToggleContext = createContext<ThemeToggleContextProps>({} as ThemeToggleContextProps);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,6 @@
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components';
-import light from '@/styles/themes/light';
-import dark from '@/styles/themes/dark';
-import GlobalStyle from '@/styles/global';
-import { useState } from 'react';
-import { ThemeToggleContext } from '@/context/ThemeToggle.context';
-import { usePersistedState } from '@/utils/usePersistedState';
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  const [theme, setTheme] = usePersistedState('theme', light);
-
-  const toggleTheme = () => {
-    setTheme(theme.title === "light" ? dark : light);
-  }
-
-  return (
-    <ThemeToggleContext.Provider value={{ toggleTheme }}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </ThemeToggleContext.Provider>
-  )
+  return <Component {...pageProps} />
 }
