a235c09f37f9237f97aff3022e0d248029875dc8
Daniel-D3V <contatodanielsilvaoficial@gmail.com>
2023-06-26 18:49:50 -0300

    finishing dasboard

--- src/pages/dashboard/annoucements/Molecules/CardProduct/Top/styles.ts
+++ src/pages/dashboard/annoucements/Molecules/CardProduct/Top/styles.ts
@@ -2,5 +2,5 @@ import { styled } from "styled-components";
 
 export const CardProductTopStyled = styled.div`
     padding-bottom: 10px;
-    border-bottom: 2px solid ${props => props.theme.borders.primary.color};
+    border-bottom: 2px solid ${props => props.theme.borders.secondary.color};
 `
